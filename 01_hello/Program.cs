e772a23ecfdf2cc7962886aba0fe5afa9ddb4eb1
benedictusdps <benedictusdps@gmail.com>
2022-04-10 22:13:18 +0700

    Add files from upload

--- 01_hello/Program.cs
+++ 01_hello/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("This is my first attempt on using C#.");
+Console.WriteLine("I want to be a game developer.");
